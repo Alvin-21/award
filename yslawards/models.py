532952bac313e7f1299f6f5f87945f67ff4536e3
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-31 13:38:31 +0300

    Add Project model.

--- yslawards/models.py
+++ yslawards/models.py
@@ -4,3 +4,9 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    image = CloudinaryField('image', null=True)
+    description = models.CharField(max_length=1500)
+    link = models.CharField(max_length=100)
