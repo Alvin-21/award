617c569cccc439af67eaeff05d64efd65ac4634e
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-31 14:52:05 +0300

    Add methods for the project class.

--- yslawards/models.py
+++ yslawards/models.py
@@ -12,6 +12,20 @@ class Project(models.Model):
     description = models.CharField(max_length=1500)
     link = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def get_project_by_id(cls, id):
+        project = cls.objects.get(id=id)
+        return project
+
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
