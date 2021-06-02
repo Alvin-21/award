6ca7da0dbeb86d036e1e47af7e2cb2432f0b1c7f
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-02 16:32:18 +0300

    Add view for the homepage.

--- yslawards/views.py
+++ yslawards/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from .models import *
+from .forms import *
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'index.html')
+    projects = Project.objects.all()
+    ratings = Ratings.objects.all()
+    form = RatingsForm()
+
+    return render(request, 'index.html', {"form": form})
+
+
