da9005c8ee4801505fc8960992fd0445d5c48311
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-08 15:07:51 +0300

    add url for profile.

--- yslawards/urls.py
+++ yslawards/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     re_path(r'^$', views.index, name='homepage'),
     re_path(r'^project/rate/(\d+)$', views.rate, name='rate'),
     re_path(r'^new/project$', views.new_project, name='new_project'),
+    re_path(r'^profile/(\d+)$', views.profile, name='profile'),
 ]
