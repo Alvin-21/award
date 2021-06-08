5ade061ef4a1557af41209f631e71040aba235e8
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-08 14:40:35 +0300

    Add url for adding a new project.

--- yslawards/urls.py
+++ yslawards/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='homepage'),
-    re_path(r'^/project/rate/(\d+)$', views.rate, name='rate'),
+    re_path(r'^project/rate/(\d+)$', views.rate, name='rate'),
+    re_path(r'^new/project$', views.new_project, name='new_project'),
 ]
