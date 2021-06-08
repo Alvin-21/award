ee72841bf90b0c95bf0a411c717d118907a75ea1
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-08 15:55:13 +0300

    Add API endpoint for getting profiles.

--- yslawards/urls.py
+++ yslawards/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     re_path(r'^new/project$', views.new_project, name='new_project'),
     re_path(r'^profile/(\d+)$', views.profile, name='profile'),
     re_path(r'^edit/profile', views.edit_profile, name='edit_profile'),
-    re_path(r'^api/projects$', views.ProjectList.as_view())
+    re_path(r'^api/projects$', views.ProjectList.as_view()),
+    re_path(r'^api/profiles$', views.ProfileList.as_view()),
 ]
