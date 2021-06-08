c327fa768910ef6056477eb6f7ade45125315444
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-08 15:14:51 +0300

    Add url for editing profile.

--- yslawards/urls.py
+++ yslawards/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     re_path(r'^project/rate/(\d+)$', views.rate, name='rate'),
     re_path(r'^new/project$', views.new_project, name='new_project'),
     re_path(r'^profile/(\d+)$', views.profile, name='profile'),
+    re_path(r'^edit/profile', views.edit_profile, name='edit_profile'),
 ]
