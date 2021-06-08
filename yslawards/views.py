daf17386b1d47b1cd52f5af4f4347bd4f7946ed7
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-08 11:16:47 +0300

    Add method for rating a project.

--- yslawards/views.py
+++ yslawards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import *
 from .forms import *
@@ -14,3 +14,18 @@ def index(request):
     return render(request, 'index.html', {"form": form})
 
 
+def rate(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    user = request.user
+
+    if request.method == 'POST':
+        form = RatingsForm(request.POST)
+        if form.is_valid():
+            design = form.cleaned_data['design']
+            usability = form.cleaned_data['usability']
+            content = form.cleaned_data['content']
+            text = form.cleaned_data['text']
+            review = form.save(commit=False)
+            rating = Ratings(critic=user, project=project, design=design, usability=usability, content=content, text=text)
+            rating.save()
+            return redirect('home')
